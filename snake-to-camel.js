896feb932d960009b8c211838f3775271f5e8424
nguarino522 <sub4miler5@gmail.com>
2022-12-06 18:05:22 -0500

    part 2 complete

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,11 @@
-function snakeToCamel() {}
+function snakeToCamel(str) {
+    let strArray = str.split("_");
 
+    for (let i=1; i < strArray.length; i++){
+        strArray[i] = strArray[i].charAt(0).toUpperCase() + strArray[i].slice(1);
+    };
+
+    let resultStr = strArray.join("");
+
+    return resultStr;
+}
--- star-out-grid.js
+++ star-out-grid.js
@@ -1,2 +1,28 @@
 function starOutGrid(grid) {
+    let rowsWithStarsSet = new Set();
+    let colsWithStarsSet = new Set();
+    
+    for (let i=0; i < grid.length; i++){
+        for (let j=0; j < grid[i].length; j++){
+            if (grid[i][j] === "*"){
+                rowsWithStarsSet.add(i);
+                colsWithStarsSet.add(j);
+            }
+        }
+    }
+
+    let rowsWithStars = Array.from(rowsWithStarsSet);
+    let colsWithStars = Array.from(colsWithStarsSet);
+
+    rowsWithStars.forEach( row => {
+        grid[row].fill("*");
+    });
+
+    colsWithStars.forEach( col => {
+        for (let i=0; i < grid.length; i++){
+            grid[i][col] = "*";
+        }
+    });
+
+    return grid;
 }
